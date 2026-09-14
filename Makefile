8de8aae4f13002b5d0b64d34d14809e4cc68a7e9
romainZer <romain.zervo@gmail.com>
2026-09-14 17:36:12 +0200

    feat: Add sizeof_types (part 1.3)

--- Makefile
+++ Makefile
@@ -7,3 +7,9 @@ tp01-cercle:
 	mkdir -p ./tp01/out
 	gcc -Wall -Wextra -o ./tp01/out/cercle.out ./tp01/cercle.c
 	./tp01/out/cercle.out
+
+
+tp01-sizeof_types: 
+	mkdir -p ./tp01/out
+	gcc -Wall -Wextra -o ./tp01/out/sizeof_types.out ./tp01/sizeof_types.c
+	./tp01/out/sizeof_types.out
--- tp01/sizeof_types.c
+++ tp01/sizeof_types.c
@@ -1,9 +1,29 @@
 // Exercice 1.3
-
 #include <stdio.h>
 
 int main()
 {
+    printf("sizeof(char): %zu\n", sizeof(char));
+    printf("sizeof(signed char): %zu\n", sizeof(signed char));
+    printf("sizeof(unsigned char): %zu\n", sizeof(unsigned char));
+
+    printf("sizeof(short): %zu\n", sizeof(short));
+    printf("sizeof(unsigned short): %zu\n", sizeof(unsigned short));
+
+    printf("sizeof(int): %zu\n", sizeof(int));
+    printf("sizeof(unsigned int): %zu\n", sizeof(unsigned int));
+
+    printf("sizeof(long int): %zu\n", sizeof(long int));
+    printf("sizeof(unsigned long int): %zu\n", sizeof(unsigned long int));
+
+    printf("sizeof(long): %zu\n", sizeof(long));
+    printf("sizeof(long int): %zu\n", sizeof(long int));
+
+    printf("sizeof(float): %zu\n", sizeof(float));
+
+    printf("sizeof(double): %zu\n", sizeof(double));
+
+    printf("sizeof(long double): %zu\n", sizeof(long double));
 
     return 0;
 }
