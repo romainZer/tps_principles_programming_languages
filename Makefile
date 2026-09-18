c053177cfb8d0b41cd86a97bebf0d130f2acea72
romainZer <romain.zervo@gmail.com>
2026-09-18 17:07:24 +0200

    feat: Add variables (part 1.4)

--- Makefile
+++ Makefile
@@ -8,8 +8,12 @@ tp01-cercle:
 	gcc -Wall -Wextra -o ./tp01/out/cercle.out ./tp01/cercle.c
 	./tp01/out/cercle.out
 
-
 tp01-sizeof_types: 
 	mkdir -p ./tp01/out
 	gcc -Wall -Wextra -o ./tp01/out/sizeof_types.out ./tp01/sizeof_types.c
 	./tp01/out/sizeof_types.out
+
+tp01-variables: 
+	mkdir -p ./tp01/out
+	gcc -Wall -Wextra -o ./tp01/out/variables.out ./tp01/variables.c
+	./tp01/out/variables.out
---
+++ tp01/variables.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main()
+{
+    char c = -100;
+    short s = -30000;
+    int i = -2000000000;
+    long int li = -1234567890L;
+    long long int lli = -1234567890123456789LL;
+    unsigned char uc = UCHAR_MAX;
+    unsigned short us = USHRT_MAX;
+    unsigned int ui = UINT_MAX;
+    unsigned long int uli = ULONG_MAX;
+    unsigned long long int ulli = ULLONG_MAX;
+    float f = -3.14f;
+    double d = 3.14159265358979;
+    long double ld = -3.14159265358979323846L;
+
+    printf("char            : %d\n", c);
+    printf("short           : %hd\n", s);
+    printf("int             : %d\n", i);
+    printf("long int        : %ld\n", li);
+    printf("long long int   : %lld\n", lli);
+    printf("unsigned char       : %hhu\n", uc);
+    printf("unsigned short      : %hu\n", us);
+    printf("unsigned int        : %u\n", ui);
+    printf("unsigned long int   : %lu\n", uli);
+    printf("unsigned long long  : %llu\n", ulli);
+    printf("float       : %f\n", f);
+    printf("double      : %lf\n", d);
+    printf("long double : %Lf\n", ld);
+
+    return 0;
+}
